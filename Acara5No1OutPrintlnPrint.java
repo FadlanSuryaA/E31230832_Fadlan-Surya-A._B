07fc4ed0f478ddc678f9cbb5a42659c8fe6330f0
icikiwir <fadlansurya12@gmail.com>
2024-01-29 12:17:55 +0700

    Create Acara5No1OutPrintlnPrint.java

---
+++ Acara5No1OutPrintlnPrint.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package acara.pkg5.no1.out.println.print;
+
+/**
+ *
+ * @author Fadlan Surya A
+ */
+public class Acara5No1OutPrintlnPrint {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("1. println ");
+        System.out.println("2. println ");
+        
+        System.out.print("1. print ");
+        System.out.print("2. print ");
+    }
+    
+}
